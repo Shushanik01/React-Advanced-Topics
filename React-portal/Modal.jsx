40d2560770bfd77cd11030128c48228414f4d594
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-06-08 21:27:34 +0400

    chore: focus added

--- React-portal/Modal.jsx
+++ React-portal/Modal.jsx
@@ -1,14 +1,36 @@
+import { useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
 export default function Modal({ open, children, onClose }) {
+
+    const portalRef = useRef(null);
+
+    useEffect(()=>{
+        portalRef.current?.focus();
+
+        const handleKeyDown = (e)=>{
+            if(e.key === 'Escape')
+                onClose()
+        }
+
+        document.addEventListener("keydown", handleKeyDown)
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown)
+        }
+
+    },[onClose])
+
+
     if (!open) return null
     else {
         return ReactDOM.createPortal(
-            
-                <div>
-                    <button onClick={onClose}>Close</button>
-                    {children}
-                </div>,
+
+
+            <div>
+                <button onClick={onClose}>Close</button>
+                {children}
+            </div>,
             document.getElementById('portal')
         )
     }

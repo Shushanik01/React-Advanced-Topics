a90c95ddfe2cfe235ebfaeb55e9292498b0cc295
Շուշանիկ Աraakelyan <shushanikarakl62@gmail.com>
2026-06-10 22:16:29 +0400

    fix: fix store config

--- Redux/store/store.ts
+++ Redux/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import userDataReducer from '../slices/silce'
 
 const store = configureStore({
     reducer:{
-
+        userData: userDataReducer
     }
 })
 export type Rootstate = ReturnType<typeof store.getState>;
-export type AppDispatch = ReturnType<typeof store.dispatch>
+export type AppDispatch = typeof store.dispatch
+export default store
